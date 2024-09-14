dcc3e207dbc6526fb972f4f5bfeffa44cbddd2ca
Georginio-prod <etonameklou6@gmail.com>
2024-09-14 22:45:57 +0000

    style: add hover for the navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                hov: '#d87d4a',
+            }
+        },
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
