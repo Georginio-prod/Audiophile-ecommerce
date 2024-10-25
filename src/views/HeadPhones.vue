a72ff59e080311a45447c9abe65e6a1b6919e596
Georginio-prod <etonameklou6@gmail.com>
2024-10-25 15:42:48 +0000

    style: add the image and text for the component

--- src/views/HeadPhones.vue
+++ src/views/HeadPhones.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <heade/>
+    <Content/>
   </div>
 </template>
 
 <script setup>
 
 import heade from '@/components/heade.vue';
+import Content from '@/components/headphone/content.vue';
 </script>
